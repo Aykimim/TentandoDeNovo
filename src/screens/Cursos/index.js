6a09d2c8f702d0fb490530879efaba4db9096a8e
Aykimim <eykimim@hotmail.com>
2023-11-03 14:45:24 -0300

    03112023

--- src/screens/Cursos/index.js
+++ src/screens/Cursos/index.js
@@ -1,8 +1,57 @@
 import React from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Image
+} from "react-native";
+import { Header } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { Container, SendButton, SendButtontext } from "./styles";
+import {
+  TextEscrita,
+  Container,
+  ButtonText,
+  ButtonCurso, // Importe ButtonCurso do styles.js
+  SendButtontext,
+  IconImage,
+  ButtonCursoPressed,
+  ButtonTextPressed
+} from "./styles";
+
+const botaoteste = () => {
+  alert("Teste");
+};
+
+function navigateToVoltar() {
+  navigation.navigate("Login");
+}
+
+function MyCustomLeftComponent() {
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <TouchableOpacity onPress={botaoteste}>
+        <Icon name="arrow-left" size={40} color="#000" />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function MyCustomCenterComponent() {
+  return (
+    // Personalize o componente de acordo com suas necessidades
+    <ButtonText>Cursos</ButtonText>
+  );
+}
+
+function MyCustomRightComponent() {
+  return (
+    // Personalize o componente de acordo com suas necessidades
+    <ButtonText></ButtonText>
+  );
+}
 
 export default function App() {
   const navigation = useNavigation();
@@ -10,15 +59,50 @@ export default function App() {
   function navigateToLogin() {
     navigation.navigate("Login");
   }
+
   return (
     <Container>
-      <View>
-        <Text>Corsos!</Text>
-      </View>
+      <Header
+        backgroundColor="#E16539"
+        leftComponent={<MyCustomLeftComponent />}
+        centerComponent={<MyCustomCenterComponent />}
+        rightComponent={<MyCustomRightComponent />}
+      />
 
-      <SendButton onPress={navigateToLogin}>
-        <SendButtontext>login</SendButtontext>
-      </SendButton>
+      <ButtonCurso onPress={botaoteste} underlayColor="#E16539">
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <IconImage source={require("../../Components/img/LogoQuadrada.png")} />
+        <ButtonText>Empreendedorismo</ButtonText>
+        </View>
+      </ButtonCurso>
+
+      <ButtonCurso onPress={botaoteste} underlayColor="#E16539">
+  <View style={{ flexDirection: "row", alignItems: "center" }}>
+    <IconImage source={require("../../Components/img/LogoQuadrada.png")} />
+    <ButtonText>Finanças</ButtonText>
+  </View>
+</ButtonCurso>
+
+      <ButtonCurso onPress={botaoteste} underlayColor="#E16539">
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <IconImage source={require("../../Components/img/LogoQuadrada.png")} />
+        <ButtonText>Marketing</ButtonText>
+        </View>
+      </ButtonCurso>
+
+      <ButtonCurso onPress={botaoteste} underlayColor="#E16539">
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <IconImage source={require("../../Components/img/LogoQuadrada.png")} />
+        <ButtonText>Gestão de RH</ButtonText>
+        </View>
+      </ButtonCurso>
+
+      <ButtonCurso onPress={botaoteste} underlayColor="#E16539">
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <IconImage source={require("../../Components/img/LogoQuadrada.png")} />
+        <ButtonText>Planejamento</ButtonText>
+        </View>
+      </ButtonCurso>
     </Container>
   );
 }
