134e76ed0adf2792fdab326ecd38f0c3f2ac3483
Aykimim <eykimim@hotmail.com>
2023-12-10 21:26:06 -0300

    anima

--- src/screens/AreaDeTests/Test/index.js
+++ src/screens/AreaDeTests/Test/index.js
@@ -1,91 +1,76 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Alert, StyleSheet } from "react-native";
-import { Header } from "react-native-elements";
-import Icon from "react-native-vector-icons/FontAwesome";
-import { colors } from "../../../Components/Theme";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDecay,
+} from 'react-native-reanimated';
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
 
-const opcoes = ["Redução da pegada de carbono e otimização de recursos.", "Impacto positivo nas comunidades e responsabilidade social.", "Atração de consumidores comprometidos com práticas sustentáveis"];
-const correspondencias = ["Tecnologias Verdes", "Inovação Social", "Educação e Conscientização"];
+const SIZE = 120;
 
 export default function App() {
-  const [correspondenciaSelecionada, setCorrespondenciaSelecionada] = useState(Array(correspondencias.length).fill(null));
+  const offset = useSharedValue(0);
+  const width = useSharedValue(0);
 
-  const verificarResposta = (opcao, index) => {
-    const novaCorrespondencia = [...correspondenciaSelecionada];
-    novaCorrespondencia[index] = opcao;
-    setCorrespondenciaSelecionada(novaCorrespondencia);
-
-    // Verificar se todas as correspondências foram feitas
-    if (!novaCorrespondencia.includes(null)) {
-      validarRespostas(novaCorrespondencia);
-    }
+  const onLayout = (event) => {
+    width.value = event.nativeEvent.layout.width;
   };
 
-  const validarRespostas = (respostas) => {
-    const respostasCorretas = ["Tecnologias Verdes", "Inovação Social", "Educação e Conscientização"];
-    
-    // Verificar se as respostas são corretas
-    if (JSON.stringify(respostas) === JSON.stringify(respostasCorretas)) {
-      Alert.alert("Respostas corretas!", "Parabéns!");
-    } else {
-      Alert.alert("Respostas incorretas!", "Tente novamente.");
-      // Reiniciar as respostas
-      setCorrespondenciaSelecionada(Array(correspondencias.length).fill(null));
-    }
-  };
+  const pan = Gesture.Pan()
+    .onChange((event) => {
+      // highlight-next-line
+      offset.value += event.changeX;
+    })
+    .onFinalize((event) => {
+      // highlight-start
+      offset.value = withDecay({
+        velocity: event.velocityX,
+        rubberBandEffect: true,
+        clamp: [-(width.value / 2) + SIZE / 2, width.value / 2 - SIZE / 2],
+      });
+      // highlight-end
+    });
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ translateX: offset.value }],
+  }));
 
   return (
-    <View>
-      <Header
-        backgroundColor={colors.primaria}
-        leftComponent={<Icon name="arrow-left" size={20} color={colors.branco} />}
-        centerComponent={<Text style={{ fontSize: 20 }}>Questão de Ligar os Pontos</Text>}
-        rightComponent={<View />}
-      />
-      <View style={{ padding: 20 }}>
-        {correspondencias.map((correspondencia, index) => (
-          <View key={index} style={styles.caixaDestino}>
-            <Text style={{ fontSize: 18 }}>{correspondencia}</Text>
-            <TouchableOpacity
-              style={[styles.caixaOpcao, { opacity: correspondenciaSelecionada[index] ? 0.5 : 1 }]}
-              onPress={() => verificarResposta(correspondencia, index)}
-              disabled={correspondenciaSelecionada[index] !== null}
-            >
-              <Text>{correspondenciaSelecionada[index] || "Arraste aqui"}</Text>
-            </TouchableOpacity>
-          </View>
-        ))}
-        <View style={{ marginTop: 20 }}>
-          {opcoes.map((opcao, opcaoIndex) => (
-            <TouchableOpacity
-              key={opcaoIndex}
-              style={styles.caixaOpcao}
-              onPress={() => verificarResposta(opcao, correspondenciaSelecionada.indexOf(null))}
-              disabled={correspondenciaSelecionada.includes(opcao)}
-            >
-              <Text>{opcao}</Text>
-            </TouchableOpacity>
-          ))}
-        </View>
+    <GestureHandlerRootView style={styles.container}>
+      <View onLayout={onLayout} style={styles.wrapper}>
+        <GestureDetector gesture={pan}>
+          <Animated.View style={[styles.box, animatedStyles]} />
+        </GestureDetector>
       </View>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  caixaDestino: {
-    borderWidth: 1,
-    borderColor: colors.primaria,
-    borderRadius: 5,
-    padding: 10,
-    marginVertical: 10,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
   },
-  caixaOpcao: {
-    backgroundColor: colors.fundo,
-    borderWidth: 1,
-    borderColor: colors.primaria,
-    borderRadius: 5,
-    padding: 10,
-    marginVertical: 5,
+  wrapper: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    height: SIZE,
+    width: SIZE,
+    backgroundColor: '#b58df1',
+    borderRadius: 20,
+    cursor: 'grab',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
