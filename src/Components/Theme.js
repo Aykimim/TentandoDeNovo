325d74ac34c9ae591e83555f2028a52b55743fb8
Aykimim <eykimim@hotmail.com>
2023-12-11 10:45:02 -0300

    temes

--- src/Components/Theme.js
+++ src/Components/Theme.js
@@ -1,79 +1,78 @@
 // Theme.js
-// theme.js
-export const colors = {
-    primaria: '#f2ce57',
-    secundaria: '#f2e3b6',
-    fundo:'#EAE8E9',
-    textoBranco: '#EAE8E9',
-    textoPreto: '#1C191A',
-    branco: '#fff',
-    trilha: '#000',
-    barraDeProgresso: '#f2ce57',
-    transparent:'#EAE8E9',
-    barraDeProgressoRedonda: '#fff',
-    barraDeProgressoRedondaTexto: '#fff',
-    tarefaFeita: '#f2ce57',
-    tarefaNaoFeita: '#f2e3b6',
-    icone:'#000',
-    // Adicione outras cores conforme necessário
-  };
-  //import { colors } from "../../../Components/Theme";
-  //no codigo é assim color: color={colors.branco}
-  //no Styles é assim color: ${colors.textoPreto};
+//lightTheme
+//import { colors } from "../../../Components/Theme";
+//no codigo é assim color: color={colors.branco}
+//no Styles é assim color: ${colors.textoPreto};
 //rgba(255,255,255,0.0)
-export const lightTheme = {
-  colors: {
-    primaria: '#f2ce57',
-    secundaria: '#f2e3b6',
-    fundo: '#EAE8E9',
-    textoBranco: '#EAE8E9',
-    textoPreto: '#1C191A',
-    branco: '#fff',
-    trilha: '#000',
-    barraDeProgresso: '#f2ce57',
-    transparent: '#EAE8E9',
-    barraDeProgressoRedonda: '#fff',
-    barraDeProgressoRedondaTexto: '#fff',
-    tarefaFeita: '#f2ce57',
-    tarefaNaoFeita: '#f2e3b6',
-    icone: '#000',
-  },
+export const colors = {
+  primaria: "#f2ce57",
+  secundaria: "#f2e3b6",
+  fundo: "#EAE8E9",
+  textoBranco: "#EAE8E9",
+  textoPreto: "#1C191A",
+  branco: "#fff",
+  trilha: "#000",
+  barraDeProgresso: "#f2ce57",
+  transparent: "#EAE8E9",
+  barraDeProgressoRedonda: "#fff",
+  barraDeProgressoRedondaTexto: "#fff",
+  tarefaFeita: "#f2ce57",
+  tarefaNaoFeita: "#f2e3b6",
+  icone: "#000"
 };
-
-export const darkTheme = {
-  colors: {
-    primaria: '#754a00',
-    secundaria: '#594e34',
-    fundo: '#1C191A',
-    textoBranco: '#1C191A',
-    textoPreto: '#EAE8E9',
-    branco: '#121212',
-    trilha: '#f2ce57',
-    barraDeProgresso: '#754a00',
-    transparent: '#EAE8E9',
-    barraDeProgressoRedonda: '#121212',
-    barraDeProgressoRedondaTexto: '#121212',
-    tarefaFeita: '#754a00',
-    tarefaNaoFeita: '#594e34',
-    icone: '#EAE8E9',
-  },
+//darkTheme
+export const colors2 = {
+ 
+    primaria: "#754a00",
+    secundaria: "#594e34",
+    fundo: "#1C191A",
+    textoBranco: "#1C191A",
+    textoPreto: "#EAE8E9",
+    branco: "#121212",
+    trilha: "#f2ce57",
+    barraDeProgresso: "#754a00",
+    transparent: "#EAE8E9",
+    barraDeProgressoRedonda: "#121212",
+    barraDeProgressoRedondaTexto: "#121212",
+    tarefaFeita: "#754a00",
+    tarefaNaoFeita: "#594e34",
+    icone: "#EAE8E9"
+  
 };
+//blueTheme
+export const colors3 = {
+    primaria: "#007ACC",
+    secundaria: "#4D92B8",
+    fundo: "#EAE8E9",
+    textoBranco: "#EAE8E9",
+    textoPreto: "#1C191A",
+    branco: "#fff",
+    trilha: "#000",
+    barraDeProgresso: "#007ACC",
+    transparent: "#EAE8E9",
+    barraDeProgressoRedonda: "#fff",
+    barraDeProgressoRedondaTexto: "#fff",
+    tarefaFeita: "#007ACC",
+    tarefaNaoFeita: "#4D92B8",
+    icone: "#000"
+  
+};
+//originalTheme
+export const colors4 = {
 
-export const blueTheme = {
-  colors: {
-    primaria: '#007ACC',
-    secundaria: '#4D92B8',
-    fundo: '#EAE8E9',
-    textoBranco: '#EAE8E9',
-    textoPreto: '#1C191A',
-    branco: '#fff',
-    trilha: '#000',
-    barraDeProgresso: '#007ACC',
-    transparent: '#EAE8E9',
-    barraDeProgressoRedonda: '#fff',
-    barraDeProgressoRedondaTexto: '#fff',
-    tarefaFeita: '#007ACC',
-    tarefaNaoFeita: '#4D92B8',
-    icone: '#000',
-  },
+    primaria: "#E16539",
+    secundaria: "#795833",
+    fundo: "#303030",
+    textoBranco: "#F0DFC8",
+    textoPreto: "#1C191A",
+    branco: "#fff",
+    trilha: "#000",
+    barraDeProgresso: "#E16539",
+    transparent: "#F0DFC8",
+    barraDeProgressoRedonda: "#fff",
+    barraDeProgressoRedondaTexto: "#fff",
+    tarefaFeita: "#E16539",
+    tarefaNaoFeita: "#303030",
+    icone: "#000"
+
 };
